cc2b5a0a07406d177c5ab6d843cf74718b300607
Vineeth Gutta <vineethg@udel.edu>
2018-08-31 14:15:23 -0400

    Create Problem1.c

---
+++ C/Problem1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void) {
+  int i = 0;
+  int sum = 0;
+  while(i < 1000){
+    if(i % 3 == 0){
+      sum += i;  
+    } 
+    if(i % 5 == 0){
+      sum = sum + i;
+    } 
+    if(i % 15 == 0){
+      sum = sum - i;
+    }
+    i++;
+  }
+  printf("%d", sum);
+  return 0;
+}
